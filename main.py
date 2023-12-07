6492188f5d277944c47bc85cdf51275c21b89fc8
Aleksander <alex.nuclearboy@gmail.com>
2023-12-07 12:05:29 +0100

    Fit to PEP 8 standards

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+"""
+This module contains the function get_birthdays_per_week to calculate birthdays
+of users for this and the next week.
+"""
+
 from datetime import date, datetime
 
 
 def get_birthdays_per_week(users):
+    """
+    Calculates the birthdays of users for this and the next week.
+
+    Parameters:
+    users (list): A list of users, where each user is a dictionary with 'name'
+    and 'birthday' keys.
+
+    Returns:
+    dict: A dictionary where keys are days of the week, and values are lists
+    of names of users whose birthday falls on that day of the week.
+    """
+
     if not users:
         return {}
 
     today = date.today()
+
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     birthdays = {day: [] for day in weekdays}
 
@@ -36,24 +54,23 @@ def get_birthdays_per_week(users):
                 birthdays[day_name].append(user['name'])
 
     # Remove days that do not have names
-    birthdays_dict = {}    
+    birthdays_dict = {}
     for day_of_week, names_list in birthdays.items():
         if names_list:
             birthdays_dict[day_of_week] = names_list
-    
+
     birthdays = birthdays_dict
 
     return birthdays
 
-if __name__ == "__main__":
-    users = [
-        {"name": "Jan Koum", "birthday": datetime(1976, 12, 7).date()},
-        {"name": "Bill Gates", "birthday": datetime(1955, 12, 5).date()}
-    ]
 
-    result = get_birthdays_per_week(users)
+if __name__ == "__main__":
+    users_eg = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 12, 7).date()},
+        {"name": "Bill Gates", "birthday": datetime(1955, 12, 5).date()}]
+
+    result = get_birthdays_per_week(users_eg)
     print(result)
     # Display the result
-    for day_name, names in result.items():
-        print(f"{day_name}: {', '.join(names)}")
-
+    for day_week_name, names in result.items():
+        print(f"{day_week_name}: {', '.join(names)}")
